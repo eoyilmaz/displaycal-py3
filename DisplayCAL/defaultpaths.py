eoyilmaz/displaycal-py3

commit f2ea3f28a8a170810af53b1501d5f505f9b4390d

fix deprecated "codeset" in gettext.translation() for python 3.11
The proposed commit additionally ensures backward compatibility with
older versions.

DisplayCAL/defaultpaths.py
@@ -229,9 +229,14 @@ else:
                         locale_dir = path
                         break
 
+            # codeset is deprecated with python 3.11
             try:
                 obj.translation = gettext.translation(
                     obj.GETTEXT_PACKAGE, locale_dir, codeset="UTF-8"
+                )
+            except TypeError:
+                obj.translation = gettext.translation(
+                    obj.GETTEXT_PACKAGE, locale_dir
                 )
             except IOError as exception:
                 print("XDG:", exception)

commit c7c30694bc587c9c41c97ec643788f5e91d1d8bb

DisplayCAL/defaultpaths.py: fix exception handling for gettext
f2ea3f28a8a170810af53b1501d5f505f9b4390d introduced support for python
3.11 by wrapping gettext.translation() with deprecated "codeset" into a
try-except statement.

While it does solve the problem with backward compatibility, users of
python 3.11 without locales will not be able to proceed due to lacking
exception handlers for this case.

Use of "fallback" ensures that gettext.NullTranslations() will return
in case of failed attempt to get locales, just like it used to be done
in the dedicated exception handler.

DisplayCAL/defaultpaths.py
@@ -232,15 +232,17 @@ else:
             # codeset is deprecated with python 3.11
             try:
                 obj.translation = gettext.translation(
-                    obj.GETTEXT_PACKAGE, locale_dir, codeset="UTF-8"
+                    obj.GETTEXT_PACKAGE, locale_dir, codeset="UTF-8",
+                    fallback=True
                 )
             except TypeError:
                 obj.translation = gettext.translation(
-                    obj.GETTEXT_PACKAGE, locale_dir
+                    obj.GETTEXT_PACKAGE, locale_dir, fallback=True
                 )
-            except IOError as exception:
-                print("XDG:", exception)
-                obj.translation = gettext.NullTranslations()
+
+            if obj.translation._charset is None:
+                print("XDG: unable to find locale for domain",
+                      obj.GETTEXT_PACKAGE)
                 return False
             return True
 
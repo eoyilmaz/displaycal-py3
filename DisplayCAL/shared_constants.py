4fc1a985faa2f7e046f11886743e839ee0858f59
Old Man <git@oldmanjk.com>
2024-09-19 15:26:05 -0500

    Add docstring and reorganize imports in shared_constants.py
    - Add docstring to describe the module's purpose.
    - Reorganize imports for better readability.
    - Define `isexe` before its usage.
    - Remove redundant `isexe` definition.
    - Improve code structure and maintainability.

--- DisplayCAL/shared_constants.py
+++ DisplayCAL/shared_constants.py
@@ -1,13 +1,19 @@
-from DisplayCAL.config import exe, exedir, isapp, pyfile
+"""
+This module contains shared constants and configuration settings for DisplayCAL.
 
+It includes paths and flags that are used throughout the application to
+determine the execution environment and locate necessary files and directories.
+"""
 
 import os
+import sys
 
+from DisplayCAL.config import exe, exedir, isapp, pyfile
 
+isexe = sys.platform != "darwin" and getattr(sys, "frozen", False)
 pydir = (
     os.path.normpath(os.path.join(exedir, "..", "Resources"))
     if isapp
     else os.path.dirname(exe if isexe else os.path.abspath(__file__))
 )
 pypath = exe if isexe else os.path.abspath(pyfile)
-isexe = sys.platform != "darwin" and getattr(sys, "frozen", False)

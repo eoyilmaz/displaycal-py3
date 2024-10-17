69e57edba604d86c887e6e8311c0cd19a17977d0
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-10-17 14:34:40 +0100

    * [#16] Initial prototype of C-Extension independent version of `RealDisplaySizeMM` module. This needs a redesign and better implementation. But is a good proof of concept.

--- tests/test_real_display_size_mm.py
+++ tests/test_real_display_size_mm.py
@@ -17,6 +17,62 @@ except ImportError:
     pass
 
 
+@pytest.fixture(scope="function")
+def patch_subprocess(monkeypatch):
+    """Patch subprocess.
+
+    Yields:
+        Any: The patched subprocess class.
+    """
+    class Process:
+        def __init__(self, output=None):
+            self.output = output
+
+        def communicate(self):
+            return self.output, None
+
+    class PatchedSubprocess:
+        passed_args = []
+        passed_kwargs = {}
+        PIPE = None
+        output = None
+
+        @classmethod
+        def Popen(cls, *args, **kwargs):
+            cls.passed_args += args
+            cls.passed_kwargs.update(kwargs)
+            process = Process(output=cls.output)
+            return process
+
+    monkeypatch.setattr(
+        "DisplayCAL.RealDisplaySizeMM.subprocess", PatchedSubprocess
+    )
+    yield PatchedSubprocess
+
+
+@pytest.fixture(scope="function")
+def patch_argyll_util(monkeypatch):
+    """Patch argyll.
+
+    Yields:
+        Any: The patched argyll class.
+    """
+
+    class PatchedArgyll:
+        passed_util_name = []
+
+        @classmethod
+        def get_argyll_util(cls, util_name):
+            cls.passed_util_name.append(util_name)
+            return "/some/path/to/argyll_v3.3.0/bin/dispwin"
+
+    monkeypatch.setattr(
+        "DisplayCAL.RealDisplaySizeMM.argyll", PatchedArgyll
+    )
+
+    yield PatchedArgyll
+
+
 def test_real_display_size_mm():
     """Test DisplayCAL.RealDisplaySizeMM.RealDisplaySizeMM() function."""
     RealDisplaySizeMM._displays = None
@@ -69,6 +125,62 @@ def test_enumerate_displays():
     assert result[0]["x11_screen"] != ""
     # assert result[0]["xrandr_name"] != ""
     assert RealDisplaySizeMM._displays is not None
+
+
+def test__enumerate_displays_dispwin_path_is_none(monkeypatch):
+    """_enumerate_displays() dispwin path is None returns empty list."""
+    monkeypatch.setattr(
+        "DisplayCAL.RealDisplaySizeMM.argyll.get_argyll_util", lambda x: None
+    )
+    result = RealDisplaySizeMM._enumerate_displays()
+    assert result == []
+
+
+def test__enumerate_displays_uses_argyll_dispwin(patch_subprocess, patch_argyll_util):
+    """DisplayCAL.RealDisplaySizeMM._enumerate_displays() uses dispwin."""
+    PatchedSubprocess = patch_subprocess
+    PatchedSubprocess.output = DisplayData.DISPWIN_OUTPUT_1
+    PatchedArgyll = patch_argyll_util
+    assert PatchedSubprocess.passed_args == []
+    assert PatchedSubprocess.passed_kwargs == {}
+    result = RealDisplaySizeMM._enumerate_displays()
+    # assert result == DisplayData.DISPWIN_OUTPUT_1
+    assert PatchedSubprocess.passed_args != []
+    assert "dispwin" in PatchedSubprocess.passed_args[0][0]
+    assert PatchedSubprocess.passed_args[0][1] == "-v"
+    assert PatchedSubprocess.passed_args[0][2] == "-d0"
+
+
+def test__enumerate_displays_uses_argyll_dispwin_output_1(patch_subprocess, patch_argyll_util):
+    """DisplayCAL.RealDisplaySizeMM._enumerate_displays() uses dispwin."""
+    PatchedSubprocess = patch_subprocess
+    PatchedSubprocess.output = DisplayData.DISPWIN_OUTPUT_1
+    PatchedArgyll = patch_argyll_util
+    result = RealDisplaySizeMM._enumerate_displays()
+    assert isinstance(result, list)
+    assert len(result) == 1
+    assert result[0]["description"] == "Built-in Retina Display, at 0, 0, width 1728, height 1117 (Primary Display)"
+    assert result[0]["name"] == "Built-in Retina Display"
+    assert result[0]["size"] == (1728, 1117)
+    assert result[0]["pos"] == (0, 0)
+
+
+def test__enumerate_displays_uses_argyll_dispwin_output_2(patch_subprocess, patch_argyll_util):
+    """DisplayCAL.RealDisplaySizeMM._enumerate_displays() uses dispwin."""
+    PatchedSubprocess = patch_subprocess
+    PatchedSubprocess.output = DisplayData.DISPWIN_OUTPUT_2
+    PatchedArgyll = patch_argyll_util
+    result = RealDisplaySizeMM._enumerate_displays()
+    assert isinstance(result, list)
+    assert len(result) == 2
+    assert result[0]["description"] == "Built-in Retina Display, at 0, 0, width 1728, height 1117 (Primary Display)"
+    assert result[0]["name"] == "Built-in Retina Display"
+    assert result[0]["size"] == (1728, 1117)
+    assert result[0]["pos"] == (0, 0)
+    assert result[1]["description"] == "DELL U2720Q, at 1728, -575, width 3008, height 1692"
+    assert result[1]["name"] == "DELL U2720Q"
+    assert result[1]["size"] == (3008, 1692)
+    assert result[1]["pos"] == (1728, -575)
 
 
 def test_get_display():

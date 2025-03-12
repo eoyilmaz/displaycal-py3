23152bd037b12dd47598de5ea85a407b6de3e0e8
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2025-03-12 02:04:18 +0000

    [#414] Added debug information for the failing test of `tests.test_ICCProfile.test_iccprofile_from_rgb_space` under Windows.

--- tests/test_ICCProfile.py
+++ tests/test_ICCProfile.py
@@ -103,6 +103,13 @@ def test_iccprofile_from_rgb_space():
         "Checksum",
         "0x{}".format(binascii.hexlify(icc.ID).upper().decode()),
     ]
+    print("result:")
+    print("=======")
+    print(result)
+    print("\n")
+    print("expected_result:")
+    print("===============")
+    print(expected_result)
 
     assert result == expected_result
 

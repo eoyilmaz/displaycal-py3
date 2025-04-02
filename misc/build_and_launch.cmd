2c533a9837224756c077641803db6aa3a95257c0
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2025-04-02 18:39:30 +0100

    [#424] Updated `build_and_launch.cmd` and `build_windows_installer.cmd` to utilize `LOCALAPPDATA` environment variable instead of a hardcoded path.

--- misc/build_and_launch.cmd
+++ misc/build_and_launch.cmd
@@ -1,5 +1,5 @@
 rmdir /s /q dist
-C:\Users\eoyil\AppData\Local\Programs\Python\Python311\python.exe -m pip uninstall -y displaycal
-C:\Users\eoyil\AppData\Local\Programs\Python\Python311\python.exe -m build
-C:\Users\eoyil\AppData\Local\Programs\Python\Python311\python.exe -m pip install --upgrade dist\displaycal-3.9.14-py3-none-any.whl
-C:\Users\eoyil\AppData\Local\Programs\Python\Python311\python.exe -m DisplayCAL
+%LOCALAPPDATA%\Programs\Python\Python311\python.exe -m pip uninstall -y displaycal
+%LOCALAPPDATA%\Programs\Python\Python311\python.exe -m build
+%LOCALAPPDATA%\Programs\Python\Python311\python.exe -m pip install --upgrade dist\displaycal-3.9.14-py3-none-any.whl
+%LOCALAPPDATA%\Programs\Python\Python311\python.exe -m DisplayCAL

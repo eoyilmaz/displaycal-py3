eoyilmaz/displaycal-py3

commit 28d8ba9cc87708d25e6b04132c6d45a81680fdd6

[#552] Addressed `SIM102` linting errors on all source files.

DisplayCAL/wxaddons.py
@@ -171,27 +171,24 @@ def SetSaneGeometry(self, x=None, y=None, w=None, h=None):
             min(display_client_rect[2] - border_lr, max(w, min_w)),
             min(display_client_rect[3] - border_tb - safety_margin, max(h, min_h)),
         )
-    if None not in (x, y):
-        if not display_client_rect.ContainsXY(
-            x, y
-        ) or not display_client_rect.ContainsRect((x, y, self.Size[0], self.Size[1])):
-            # If outside client area, move into client area
-            xy = [x, y]
-            for i, pos in enumerate([xy, (x + self.Size[0], y + self.Size[1])]):
-                for j in range(2):
-                    if (
-                        pos[j] > display_client_rect[j] + display_client_rect[2 + j]
-                        or pos[j] < display_client_rect[j]
-                    ):
-                        if i:
-                            xy[j] = (
-                                display_client_rect[j]
-                                + display_client_rect[2 + j]
-                                - self.Size[j]
-                            )
-                        else:
-                            xy[j] = display_client_rect[j]
-            self.SetPosition(tuple(xy))
+    if None not in (x, y) and  not display_client_rect.ContainsXY(x, y) or not display_client_rect.ContainsRect((x, y, self.Size[0], self.Size[1])):
+        # If outside client area, move into client area
+        xy = [x, y]
+        for i, pos in enumerate([xy, (x + self.Size[0], y + self.Size[1])]):
+            for j in range(2):
+                if (
+                    pos[j] > display_client_rect[j] + display_client_rect[2 + j]
+                    or pos[j] < display_client_rect[j]
+                ):
+                    if i:
+                        xy[j] = (
+                            display_client_rect[j]
+                            + display_client_rect[2 + j]
+                            - self.Size[j]
+                        )
+                    else:
+                        xy[j] = display_client_rect[j]
+        self.SetPosition(tuple(xy))
 
 
 wx.Window.SetSaneGeometry = SetSaneGeometry
@@ -580,9 +577,8 @@ class BetterWindowDisabler:
     def enable(self, enable=True):
         if not enable:
             skip = self.skip or []
-            if skip:
-                if not isinstance(skip, (list, tuple)):
-                    skip = [skip]
+            if skip and not isinstance(skip, (list, tuple)):
+                skip = [skip]
             if self.toplevelparent:
                 toplevel = [self.toplevelparent]
             else:

DisplayCAL/RealDisplaySizeMM.py
@@ -353,9 +353,8 @@ def get_wayland_display(x, y, w, h):
 
 
 def get_x_display(display_no=0):
-    if display := get_display(display_no):
-        if name := display.get("name"):
-            return _get_x_display(name)
+    if (display := get_display(display_no)) and (name := display.get("name")):
+        return _get_x_display(name)
 
 
 def get_x_icc_profile_atom_id(display_no=0):

DisplayCAL/cgats.py
@@ -667,9 +667,8 @@ class CGATS(dict):
                         else:
                             if "KEYWORDS" in self and key in list(
                                 self["KEYWORDS"].values()
-                            ):
-                                if self.emit_keywords:
-                                    result.append(b'KEYWORD "%s"' % key.encode())
+                            ) and self.emit_keywords:
+                                result.append(b'KEYWORD "%s"' % key.encode())
                             if isinstance(value, bytes):
                                 # Need to escape single quote -> double quote
                                 value = value.replace(b'"', b'""')
@@ -850,12 +849,11 @@ class CGATS(dict):
                         # Same gray as prev value
                         prev = color
                         cur = gray
-                        if prev_i not in added:
-                            if debug:
-                                print(
-                                    "INFO - appending prev %s to color because prev was"
-                                    " same gray but got skipped" % prev_values[:3]
-                                )
+                        if prev_i not in added and debug:
+                            print(
+                                "INFO - appending prev %s to color because prev was"
+                                " same gray but got skipped" % prev_values[:3]
+                            )
                         if debug:
                             print(
                                 "INFO - appending cur to gray because prev %s was same "
@@ -865,12 +863,11 @@ class CGATS(dict):
                         # Prev value was different gray
                         prev = gray
                         cur = gray
-                        if prev_i not in added:
-                            if debug:
-                                print(
-                                    "INFO - appending prev %s to gray because prev was "
-                                    "different gray but got skipped" % prev_values[:3]
-                                )
+                        if prev_i not in added and debug:
+                            print(
+                                "INFO - appending prev %s to gray because prev was "
+                                "different gray but got skipped" % prev_values[:3]
+                            )
                         if debug:
                             print(
                                 "INFO - appending cur to gray because prev %s was "
@@ -1870,9 +1867,8 @@ Transform {
                             current_query_value = query[query_key]
                         else:
                             current_query_value = query_value
-                        if current_query_value is not None:
-                            if item[query_key] != current_query_value:
-                                break
+                        if current_query_value is not None and item[query_key] != current_query_value:
+                            break
                         if get_value:
                             result_n[len(result_n)] = item[query_key]
                         match_count += 1

DisplayCAL/chromecast_patterngenerator.py
@@ -67,9 +67,8 @@ class ChromeCastPatternGenerator:
 
     def disconnect_client(self):
         self.listening = False
-        if hasattr(self, "_cc"):
-            if self._cc.app_id:
-                self._cc.quit_app()
+        if hasattr(self, "_cc") and self._cc.app_id:
+            self._cc.quit_app()
         if hasattr(self, "conn"):
             del self.conn
 

DisplayCAL/edid.py
@@ -342,10 +342,8 @@ def get_edid_from_xrandr(display_no):
             else:
                 # read all data, exit
                 break
-        if found_display:
-            # try to find EDID
-            if b"EDID" in line:
-                found_edid = True
+        if found_display and b"EDID" in line:  # try to find EDID
+            found_edid = True
         # try to find the display
         if (display["name"] + b" connected") in line:
             found_display = True
@@ -779,15 +777,13 @@ def parse_edid_extension_blocks(edid):
         # Parse extension blocks
         block = edid[128:]
         while block:
-            if block[0] == BLOCK_DI_EXT:
-                if block[TRC[0]] != "\0":
-                    # TODO: Implement
-                    pass
+            if block[0] == BLOCK_DI_EXT and block[TRC[0]] != "\0":
+                # TODO: Implement
+                pass
             block = block[128:]
     return result
 
 
 class WMIError(Exception):
     """Custom exception for WMI errors."""
-
     pass

DisplayCAL/floatspin.py
@@ -1031,15 +1031,12 @@ class FloatSpin(wx.PyControl):
 
         :return: A clamped copy of `var`.
         """
+        if self._min is not None and float(var) < float(self._min):
+            var = self._min
+            return var
 
-        if self._min is not None:
-            if float(var) < float(self._min):
-                var = self._min
-                return var
-
-        if self._max is not None:
-            if float(var) > float(self._max):
-                var = self._max
+        if self._max is not None and float(var) > float(self._max):
+            var = self._max
 
         return var
 
@@ -1248,12 +1245,10 @@ class FloatSpin(wx.PyControl):
         """Returns the maximum value for :class:`FloatSpin`. It can be a
         number or ``None`` if no minimum is present.
         """
-
         return self._max
 
     def HasRange(self):
         """Returns whether :class:`FloatSpin` range has been set or not."""
-
         return (self._min is not None) or (self._max is not None)
 
     def InRange(self, value):
@@ -1261,20 +1256,14 @@ class FloatSpin(wx.PyControl):
 
         :param value: the value to test.
         """
-
         if not self.HasRange():
             return True
-        if self._min is not None:
-            if float(value) < float(self._min):
-                return False
-        if self._max is not None:
-            if float(value) > float(self._max):
-                return False
-        return True
+        if self._min is not None and float(value) < float(self._min):
+            return False
+        return self._max is None or float(value) <= float(self._max)
 
     def GetTextCtrl(self):
         """Returns the underlying :class:`TextCtrl`."""
-
         return self._textctrl
 
     def IsFinite(self, value):
@@ -1282,7 +1271,6 @@ class FloatSpin(wx.PyControl):
 
         :param value: the value to test.
         """
-
         try:
             snap_value = (value - self._defaultvalue) / self._increment
             finite = True
@@ -1873,9 +1861,8 @@ def get_all_keyboard_focusable_children(parent):
     else:
         for child in parent.Children:
             if child.Enabled and child.IsShownOnScreen():
-                if child.AcceptsFocusFromKeyboard():
-                    if not isinstance(child, wx.RadioButton) or child.Value:
-                        children.append(child)
+                if child.AcceptsFocusFromKeyboard() and (not isinstance(child, wx.RadioButton) or child.Value):
+                    children.append(child)
                 if child.Children:
                     children.extend(get_all_keyboard_focusable_children(child))
     return children

DisplayCAL/icc_profile.py
@@ -1290,13 +1290,12 @@ def create_synthetic_hdr_clut_profile(
         prevperc = startperc = perc = 50
 
     for i, item in enumerate(HDR_XYZ):
-        if not item:  # Aborted
-            if worker and worker.thread_abort:
-                if forward_xicclu:
-                    forward_xicclu.exit()
-                if backward_xicclu:
-                    backward_xicclu.exit()
-                raise Exception("aborted")
+        if not item and worker and worker.thread_abort:  # Aborted
+            if forward_xicclu:
+                forward_xicclu.exit()
+            if backward_xicclu:
+                backward_xicclu.exit()
+            raise Exception("aborted")
         (RGB, (X, Y, Z), RGB_ICtCp_XYZ) = item
         I, Ct, Cp = colormath.XYZ2ICtCp(X, Y, Z, oetf=eotf_inverse)
         X, Y, Z = (v / maxv for v in (X, Y, Z))
@@ -4000,10 +3999,9 @@ class CurveType(ICCProfileTag, list):
             )
         vmin = 0
         vmax = 65535.0
-        if use_vmin_vmax:
-            if len(self) > 2:
-                vmin = self[0]
-                vmax = self[-1]
+        if use_vmin_vmax and len(self) > 2:
+            vmin = self[0]
+            vmax = self[-1]
         return colormath.get_gamma(values, 65535.0, vmin, vmax, average, least_squares)
 
     def get_transfer_function(
@@ -8045,9 +8043,8 @@ class ICCProfile:
         tag table and data) on-the-fly.
         """
         if not stream_or_filename:
-            if self._file:
-                if not self._file.closed:
-                    self.close()
+            if self._file and not self._file.closed:
+                self.close()
             stream_or_filename = self.fileName
         if isinstance(stream_or_filename, str):
             with open(stream_or_filename, "wb") as stream:

commit d0c749260561b57540fc1cb950fd8da1bac3ad98

[#552] Fixed a bugs in `DisplayCAL.worker` and `DisplayCAL.wxaddons` introduced with 28d8ba9.

DisplayCAL/display_cal.py
@@ -5459,8 +5459,10 @@ class MainFrame(ReportFrame, BaseFrame, LUT3DMixin):
                 return False
         if (
             profile
-            and getcfg("profile.type") != "S"
-            or not getcfg("profile.black_point_compensation")
+            and (
+                getcfg("profile.type") != "S"
+                or not getcfg("profile.black_point_compensation")
+            )
         ):  # Warn about profile bugs
             dlg = ConfirmDialog(
                 self,
@@ -6841,7 +6843,7 @@ class MainFrame(ReportFrame, BaseFrame, LUT3DMixin):
         if self.ambient_viewcond_adjust_cb.GetValue():
             self.ambient_viewcond_adjust_textctrl.Enable()
             v = self.ambient_viewcond_adjust_textctrl.GetValue()
-            if v and v < 0.000001 or v > sys.maxsize:
+            if v and (v < 0.000001 or v > sys.maxsize):
                 wx.Bell()
                 self.ambient_viewcond_adjust_textctrl.SetValue(
                     getcfg("calibration.ambient_viewcond_adjust.lux")

DisplayCAL/wxaddons.py
@@ -173,8 +173,10 @@ def SetSaneGeometry(self, x=None, y=None, w=None, h=None):
         )
     if (
         None not in (x, y)
-        and not display_client_rect.ContainsXY(x, y)
-        or not display_client_rect.ContainsRect((x, y, self.Size[0], self.Size[1]))
+        and (
+            not display_client_rect.ContainsXY(x, y)
+            or not display_client_rect.ContainsRect((x, y, self.Size[0], self.Size[1]))
+        )
     ):
         # If outside client area, move into client area
         xy = [x, y]
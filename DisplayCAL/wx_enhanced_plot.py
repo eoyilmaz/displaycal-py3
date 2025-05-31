eoyilmaz/displaycal-py3

commit e93ccd65a71e9a9cae5f1135e68259ead8d478b9

[#552] Addressed `RUF005` linting errors.

DisplayCAL/wx_enhanced_plot.py
@@ -667,7 +667,7 @@ class PolyMarker(PolyPoints):
     def _circle(self, dc, coords, size=1):
         fact = 2.5 * size
         wh = 5.0 * size
-        rect = np.zeros((len(coords), 4), float) + [0.0, 0.0, wh, wh]
+        rect = [*np.zeros((len(coords), 4), float), 0.0, 0.0, wh, wh]
         rect[:, 0:2] = coords - [fact, fact]
         dc.DrawEllipseList(rect.astype(np.int32))
 
@@ -677,7 +677,7 @@ class PolyMarker(PolyPoints):
     def _square(self, dc, coords, size=1):
         fact = 2.5 * size
         wh = 5.0 * size
-        rect = np.zeros((len(coords), 4), float) + [0.0, 0.0, wh, wh]
+        rect = [*np.zeros((len(coords), 4), float), 0.0, 0.0, wh, wh]
         rect[:, 0:2] = coords - [fact, fact]
         dc.DrawRectangleList(rect.astype(np.int32))
 
@@ -1834,9 +1834,7 @@ class PlotCanvas(wx.Panel):
             if len(obj.points) == 0:
                 continue  # go to next obj
             # [curveNumber, legend, index of closest point, pointXY, scaledXY, distance]
-            cn = (
-                [curveNum] + [obj.getLegend()] + obj.getClosestPoint(pntXY, pointScaled)
-            )
+            cn = [curveNum, obj.getLegend(), *obj.getClosestPoint(pntXY, pointScaled)]
             closest_points.append(cn)
         return closest_points
 

DisplayCAL/argyll.py
@@ -88,7 +88,7 @@ def check_argyll_bin(paths: None | list[str] = None) -> bool:
             if argyll_dir:
                 if argyll_dir in paths:
                     paths.remove(argyll_dir)
-                paths = [argyll_dir] + paths
+                paths = [argyll_dir, *paths]
         print("[D] Search path:\n  ", "\n  ".join(paths))
     # Fedora doesn't ship Rec709.icm
     config.DEFAULTS["3dlut.input.profile"] = (
@@ -264,7 +264,7 @@ def get_argyll_util(name, paths=None):
         if argyll_dir:
             if argyll_dir in paths:
                 paths.remove(argyll_dir)
-            paths = [argyll_dir] + paths
+            paths = [argyll_dir, *paths]
     cache_key = os.pathsep.join(paths)
     exe = ARGYLL_UTILS.get(cache_key, {}).get(name, None)
     if exe:

DisplayCAL/argyll_cgats.py
@@ -606,7 +606,7 @@ def vcgt_to_cal(profile):
     context[key].type = key.encode("utf-8")
     values = profile.tags.vcgt.getNormalizedValues()
     for i, triplet in enumerate(values):
-        context[key].add_data((b"%.7f" % (i / float(len(values) - 1)),) + triplet)
+        context[key].add_data((b"%.7f" % (i / float(len(values) - 1)), *triplet))
     return cgats
 
 

DisplayCAL/argyll_names.py
@@ -105,4 +105,5 @@ INTENTS = ["a", "aa", "aw", "la", "lp", "ms", "p", "pa", "r", "s"]
 VIDEO_ENCODINGS = ["n", "t", "6", "7", "5", "2", "C", "x", "X"]
 
 # Observers
+# TODO: OBSERVERS should include 2012_2 and 2012_10.
 OBSERVERS = ["1931_2", "1955_2", "1964_10", "1964_10c", "1978_2", "shaw"]

DisplayCAL/cgats.py
@@ -2237,7 +2237,7 @@ Transform {
                 else:
                     labels = ("XYZ_X", "XYZ_Y", "XYZ_Z")
                     index = 1  # Index of Y in labels
-                data = dataset.queryi(("RGB_R", "RGB_G", "RGB_B") + labels)
+                data = dataset.queryi(("RGB_R", "RGB_G", "RGB_B", *labels))
 
                 # Get blacks
                 blacks = data.queryi({"RGB_R": 0, "RGB_G": 0, "RGB_B": 0})

DisplayCAL/config.py
@@ -215,13 +215,14 @@ UNCALIBRATABLE_DISPLAYS = ("Untethered$",)
 # Can the device generate patterns of its own?
 PATTERN_GENERATORS = ("madVR$", "Resolve$", "Chromecast ", "Prisma ", "Prisma$")
 
-NON_ARGYLL_DISPLAYS = UNCALIBRATABLE_DISPLAYS + ("Resolve$",)
+NON_ARGYLL_DISPLAYS = (*UNCALIBRATABLE_DISPLAYS, "Resolve$")
 
 # Is the device directly connected or e.g. driven via network?
 # (note that madVR can technically be both, but the endpoint is always directly
 # connected to a display so we have videoLUT access via madVR's API.
 # Only devices which don't support that are considered 'untethered' in this context)
-UNTETHERED_DISPLAYS = NON_ARGYLL_DISPLAYS + (
+UNTETHERED_DISPLAYS = (
+    *NON_ARGYLL_DISPLAYS,
     "Web$",
     "Chromecast ",
     "Prisma ",
@@ -229,7 +230,7 @@ UNTETHERED_DISPLAYS = NON_ARGYLL_DISPLAYS + (
 )
 
 # Is the device not an actual display device (i.e. is it not a TV or monitor)?
-VIRTUAL_DISPLAYS = UNTETHERED_DISPLAYS + ("madVR$",)
+VIRTUAL_DISPLAYS = (*UNTETHERED_DISPLAYS, "madVR$")
 
 
 def is_special_display(
@@ -947,31 +948,31 @@ def runtimeconfig(pyfile):
         DATA_DIRS.extend(
             [
                 os.path.join(dir_, "doc", f"{APPNAME}-{VERSION_STRING}")
-                for dir_ in XDG_DATA_DIRS + [XDG_DATA_HOME]
+                for dir_ in [*XDG_DATA_DIRS, XDG_DATA_HOME]
             ]
         )
         DATA_DIRS.extend(
             [
                 os.path.join(dir_, "doc", "packages", APPNAME)
-                for dir_ in XDG_DATA_DIRS + [XDG_DATA_HOME]
+                for dir_ in [*XDG_DATA_DIRS, XDG_DATA_HOME]
             ]
         )
         DATA_DIRS.extend(
             [
                 os.path.join(dir_, "doc", APPNAME)
-                for dir_ in XDG_DATA_DIRS + [XDG_DATA_HOME]
+                for dir_ in [*XDG_DATA_DIRS, XDG_DATA_HOME]
             ]
         )
         DATA_DIRS.extend(
             [
                 os.path.join(dir_, "doc", APPNAME.lower())  # Debian
-                for dir_ in XDG_DATA_DIRS + [XDG_DATA_HOME]
+                for dir_ in [*XDG_DATA_DIRS, XDG_DATA_HOME]
             ]
         )
         DATA_DIRS.extend(
             [
                 os.path.join(dir_, "icons", "hicolor")
-                for dir_ in XDG_DATA_DIRS + [XDG_DATA_HOME]
+                for dir_ in [*XDG_DATA_DIRS, XDG_DATA_HOME]
             ]
         )
     if DEBUG:
@@ -1083,7 +1084,7 @@ VALID_VALUES = {
     # worker.Worker.add_measurement_features if using Argyll >= 1.5
     # See http://www.argyllcms.com/doc/instruments.html for description of
     # per-instrument supported modes
-    "measurement_mode": [None, "auto"] + list(string.digits[1:] + string.ascii_letters),
+    "measurement_mode": [None, "auto", *list(string.digits[1:] + string.ascii_letters)],
     "gamap_default_intent": ["a", "r", "p", "s"],
     "gamap_perceptual_intent": INTENTS,
     "gamap_saturation_intent": INTENTS,

DisplayCAL/display_cal.py
@@ -1480,7 +1480,7 @@ class GamapFrame(BaseFrame):
                 src_viewcond = getcfg("gamap_src_viewcond")
                 if event and (
                     (
-                        src_viewcond in ([None] + self.viewconds_out_nondisplay)
+                        src_viewcond in [None, *self.viewconds_out_nondisplay]
                         and profile.profileClass in (b"mntr", b"spac")
                     )
                     or (
@@ -1702,7 +1702,7 @@ class GamapFrame(BaseFrame):
 
         self.gamap_src_viewcond_ctrl.SetItems(list(self.viewconds_ab.values()))
         self.gamap_out_viewcond_ctrl.SetItems(
-            [lang.getstr("none")] + list(self.viewconds_out_ab.values())
+            [lang.getstr("none"), *self.viewconds_out_ab.values()]
         )
 
         self.gamap_default_intent_ctrl.SetItems(
@@ -11653,14 +11653,15 @@ class MainFrame(ReportFrame, BaseFrame, LUT3DMixin):
                 self.profile_info[id_].Raise()
 
     def get_commands(self):
-        return self.get_common_commands() + [
+        return [
+            *self.get_common_commands(),
             "3DLUT-maker [create filename]",
             "calibrate",
             "calibrate-profile",
             "create-colorimeter-correction",
             "create-profile [filename]",
             "curve-viewer [filename]",
-            APPNAME + " [filename]",
+            f"{APPNAME} [filename]",
             "enable-spyder2",
             "import-colorimeter-corrections [filename...]",
             "install-profile [filename]",
@@ -12831,7 +12832,7 @@ class MainFrame(ReportFrame, BaseFrame, LUT3DMixin):
             # Bind event handlers
             def correction_type_handler(event):
                 dlg.Freeze()
-                for item in list(boxsizer.Children) + [boxsizer.StaticBox]:
+                for item in [*list(boxsizer.Children), boxsizer.StaticBox]:
                     if isinstance(item, (wx.SizerItem, wx.Window)):
                         item.Show(dlg.correction_type_matrix.GetValue())
                 matrix = dlg.correction_type_matrix.GetValue()
@@ -15634,7 +15635,7 @@ class MainFrame(ReportFrame, BaseFrame, LUT3DMixin):
             if exclude_ext:
                 args.extend(f"-xr!*{ext}" for ext in exclude_ext)
             return self.worker.exec_cmd(
-                sevenzip, args + [archive_path] + filenames, capture_output=True
+                sevenzip, [*args, archive_path, *filenames], capture_output=True
             )
         # Create gzipped TAR or ZIP archive
         dirbasename = ""
@@ -16050,7 +16051,7 @@ class MainFrame(ReportFrame, BaseFrame, LUT3DMixin):
                             ti3_file.write(b"\n".join(ti3_lines))
                         collected_paths.append(collected_path)
                     # Average the TI3 files
-                    args = ["-v"] + collected_paths + [ti3_tmp_path]
+                    args = ["-v", *collected_paths, ti3_tmp_path]
                     cmd = get_argyll_util("average")
                     result = self.worker.exec_cmd(
                         cmd, args, capture_output=True, skip_scripts=True
@@ -16899,15 +16900,14 @@ class MainFrame(ReportFrame, BaseFrame, LUT3DMixin):
             force
             or (
                 lang.getstr(os.path.basename(path))
-                in [""] + self.default_testchart_names
+                in ["", *self.default_testchart_names]
             )
             or not os.path.isfile(path)
         ):
-            if (
-                not force
-                and lang.getstr(os.path.basename(path))
-                in [""] + self.default_testchart_names
-            ):
+            if not force and lang.getstr(os.path.basename(path)) in [
+                "",
+                *self.default_testchart_names,
+            ]:
                 ti1 = os.path.basename(path)
             else:
                 ti1 = self.testchart_defaults[self.get_profile_type()].get(
@@ -17130,7 +17130,7 @@ class MainFrame(ReportFrame, BaseFrame, LUT3DMixin):
                     self.testcharts.append(
                         os.pathsep.join((testchart_name, testchart_dir))
                     )
-        self.testcharts = ["auto"] + natsort(self.testcharts)
+        self.testcharts = ["auto", *natsort(self.testcharts)]
         self.testchart_names = []
         for i, chart in enumerate(self.testcharts):
             chart = chart.split(os.pathsep)
@@ -19114,7 +19114,7 @@ class StartupFrame(start_cls):
             gamma = 2.04  # somewhat arbitrary gamma value, but works the best for macOS
             if self.worker.exec_cmd(
                 screencap,
-                extra_args + ["screencap.png"],
+                [*extra_args, "screencap.png"],
                 capture_output=True,
                 skip_scripts=True,
                 silent=True,

DisplayCAL/freeze.py
@@ -506,7 +506,7 @@ def build_py2exe():
         f"{appname.lower()}-apply-profiles-launcher",
         f"{appname} Profile Loader Launcher",
     )
-    for script, _desc in scripts + [apply_profiles_launcher]:
+    for script, _desc in [*scripts, apply_profiles_launcher]:
         shutil.copy(
             os.path.join(source_dir, "scripts", script),
             os.path.join(tmp_scripts_dir, script2pywname(script)),
@@ -696,7 +696,7 @@ def build_py2exe():
         # manifest_in.append("include " + os.path.join(pkgdir, "*.so"))
         for obj in attrs.get("package_data", {}).get(pkg, []):
             print(f"obj: {obj}")
-            manifest_in.append("include " + os.path.sep.join([pkgdir] + obj.split("/")))
+            manifest_in.append(f"include {os.path.sep.join([pkgdir, *obj.split('/')])}")
     manifest_in.extend(
         "include {}".format(os.path.join(*pymod.split(".")))
         for pymod in attrs.get("py_modules", [])

commit 40b0feced106be9ae82db98f5749dff6ca4dd68d

[#552] Fixed bug in `DisplayCAL.wx_enhanced_plot` introduced with e93ccd6.

DisplayCAL/wx_enhanced_plot.py
@@ -839,7 +839,7 @@ class PolyMarker(PolyPoints):
     def _circle(self, dc, coords, size=1):
         fact = 2.5 * size
         wh = 5.0 * size
-        rect = [*np.zeros((len(coords), 4), float), 0.0, 0.0, wh, wh]
+        rect = np.zeros((len(coords), 4), float) + [0.0, 0.0, wh, wh]  # noqa: RUF005
         rect[:, 0:2] = coords - [fact, fact]
         dc.DrawEllipseList(rect.astype(np.int32))
 
@@ -849,7 +849,7 @@ class PolyMarker(PolyPoints):
     def _square(self, dc, coords, size=1):
         fact = 2.5 * size
         wh = 5.0 * size
-        rect = [*np.zeros((len(coords), 4), float), 0.0, 0.0, wh, wh]
+        rect = np.zeros((len(coords), 4), float) + [0.0, 0.0, wh, wh]  # noqa: RUF005
         rect[:, 0:2] = coords - [fact, fact]
         dc.DrawRectangleList(rect.astype(np.int32))
 